8486797dc8a63bfdd759d924d1382f17a7d15085
Cecilia Lindström <cecilia.a.lindstrom@live.se>
2024-05-13 18:33:23 +0200

    definierat model för courses och använt httpclient i service-filen för courses

--- src/app/services/coursedata.service.ts
+++ src/app/services/coursedata.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Courses } from '../model/courses';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursedataService {
 
-  constructor() { }
+  //url till json-flien som innehåller kurserna
+
+  private url:string = "assets/miun_courses.json";
+
+  //httpclient för anrop
+  constructor(private http: HttpClient) { }
+
+  //funktion för att hämta in kurserna
+
+  getCourses():Observable<Courses[]> {
+    return this.http.get<Courses[]>(this.url);
+  }
 }
