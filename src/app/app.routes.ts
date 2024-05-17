e3e35886a5d1f22e4662e80a47df395301170e6d
Cecilia Lindström <cecilia.a.lindstrom@live.se>
2024-05-17 09:31:20 +0200

    routing + home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { CoursesComponent } from './courses/courses.component';
 import { FrameworkComponent } from './framework/framework.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     { path: 'courses', component: CoursesComponent},
-    { path: 'framework', component: FrameworkComponent}
+    { path: 'framework', component: FrameworkComponent},
+    { path: 'home', component: HomeComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
